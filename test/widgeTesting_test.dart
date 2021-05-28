df6011560df8b1af2ef3f3d85f65bc116005a5e9
RaynaNoveloisaBaoy <81216151+RaynaNoveloisaBaoy@users.noreply.github.com>
2021-05-28 09:16:01 -0700

    Update widgeTesting_test.dart

--- test/widgeTesting_test.dart
+++ test/widgeTesting_test.dart
@@ -4,14 +4,14 @@ import 'package:wap/welcome.dart';
 void main() {
 testWidgets('WelcomePage has a title and message', (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
-     await tester.pumpWidget(_WelcomePageState(title: 'T', child: 'M'));
+     await tester.pumpWidget(WelcomePage(title: 'T'));
 
     // Create the Finders.
     final titleFinder = find.text('T');
-    final childFinder = find.text('M');
+   
    
 
     expect(titleFinder, findsOneWidget);
-    expect(childFinder, findsOneWidget);
+    
   });
 }
