990a6c16a4271a065c48b513b8c0ce96860faf0a
RaynaNoveloisaBaoy <81216151+RaynaNoveloisaBaoy@users.noreply.github.com>
2021-05-28 08:48:16 -0700

    Add files via upload

---
+++ test/Integration Testing.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wap/welcome_slider.dart';
+
+void main() {
+	
+
+
+testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+     await tester.pumpWidget(MyWidget(title: 'T', description: 'M'));
+
+    // Create the Finders.
+    final titleFinder = find.text('T');
+    final descriptionFinder = find.text('M');
+   
+
+    expect(titleFinder, findsWidgets);
+    expect(descriptionFinder, findsWidgets);
+  });
+}
