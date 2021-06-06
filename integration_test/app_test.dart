8a501b38c1886ff6cfcd8dc2a6a7760d09468544
RaynaNoveloisaBaoy <81216151+RaynaNoveloisaBaoy@users.noreply.github.com>
2021-06-06 03:33:29 -0700

    summary

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,10 +1,11 @@
+// ignore: unused_import
+import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+// ignore: unused_import
+import 'package:wap/welcome.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets("failing test example", (WidgetTester tester) async {
-    expect(2 + 2, equals(5));
-  });
 }
